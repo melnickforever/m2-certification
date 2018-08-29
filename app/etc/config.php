61dfecdf609feb2f16a55fe7dec2829743ad1de5
Dmytro Melnyk <d.melnyk@ism-ukraine.com>
2018-08-29 14:35:16 +0300

    test command with options

--- app/etc/config.php
+++ app/etc/config.php
@@ -3,6 +3,7 @@ return array (
   'modules' => 
   array (
     'Dmelnyk_BlockViewModel' => 1,
+    'Dmelnyk_Command' => 1,
     'Magento_Store' => 1,
     'Magento_Directory' => 1,
     'Magento_AdvancedPricingImportExport' => 1,
