59e050711848a835ff67a3688bfa94ba1e9afb49
Dmytro Melnyk <d.melnyk@ism-ukraine.com>
2018-09-06 14:33:58 +0300

    added ui frontend ui component

---
+++ Dmelnyk/UiComponent/Block/FrontendUiComponentBlock.php
@@ -0,0 +1,12 @@
+<?php
+namespace Dmelnyk\UiComponent\Block;
+
+
+class FrontendUiComponentBlock extends \Magento\Framework\View\Element\Template
+{
+    public function getTitle()
+    {
+        return "Hello World";
+    }
+
+}
